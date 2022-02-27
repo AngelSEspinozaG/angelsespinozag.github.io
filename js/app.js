2fce5e98ad66c77e1c73254308c933d0e00a9e10
Ángel Salvador Espinoza Granda <100351740+AngelSEspinozaG@users.noreply.github.com>
2022-02-27 21:34:45 +0100

    Angel Espinoza
    mi web

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var check=document.querySelector(".check");
+check.addEventListener('click',idioma)
+function idioma(){
+    let id=check.checked;
+    if (id==true){
+        location.href="./index1/index.html";
+    }else{
+        location.href="../index.html";
+    }
+}
